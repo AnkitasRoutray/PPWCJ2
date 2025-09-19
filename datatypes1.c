b80f18dcd4499c624753f855a79594b772ae0b79
AnkitasRoutray <ankitaroutray2005@gmail.com>
2025-09-20 00:02:23 +0530

    Add files via upload

---
+++ datatypes1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  
+    int main(){
+    
+    int i=3;
+    float f=6.7f;
+    double d=66.77;
+    char c='S';
+
+ 
+ printf("Integer value: %d\n", i);
+    printf("Float value: %f\n", f);
+    printf("Double value: %.f\n", d);
+    printf("Character: %c\n", c);
+
+       return 0;
+  
+  }
